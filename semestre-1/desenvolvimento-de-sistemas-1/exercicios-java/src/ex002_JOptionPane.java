f6b402802285f7684e8083fcb5b163b7c10e564e
Matheus Massuda <mat.massuda@gmail.com>
2025-03-17 17:06:26 -0300

    criação de diretórios e atividades e exercícios
    - criação atividade03-forum-comunicacao;
    - criação atividade03-comunicacao;
    - criação atividade04-ds1;
    - upload materiais comunicação, ti1 e ds1 (.PDF);
    - criação exercícios-java;

---
+++ semestre-1/desenvolvimento-de-sistemas-1/exercicios-java/src/ex002_JOptionPane.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class ex002_JOptionPane {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Primeiro programa em java.");
+    }
+}
