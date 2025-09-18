6cc6a52c75e1687b3bd356c8004333df5d57c4e9
Matheus Massuda <mat.massuda@gmail.com>
2025-09-18 09:37:20 -0300

    chore: criação arquivos php
    - Criado: mini projeto CRUD (Create | Read | Update | Delete) com MySQL e PHP;

---
+++ semestre_2/Desenvolvimento_de_Sistemas_2/Atividades/Aula07/Agenda07/cadastroAction.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <title>Cadastro - PDO</title>
+</head>
+<body class="w3-black">
+
+    <div class="w3-padding w3-content w3-text-grey w3-third w3-display-middle">
+
+        <!--Declaração de variáveis comc redenciais do BD-->
+        <?php
+        $host = "localhost";
+        $usuario = "pwii_user";
+        $senha = "senha123";
+        $bd = "pwii";
+
+        //Tenta criar um objeto PDO (conexão com MySQL)
+        try{
+        $conecta = new PDO("mysql:dbname=$bd; host=$host; port=3306; chars
+        et=utf8", $usuario, $senha);
+        $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+        echo "falha ao conectar: ". $e->getMessage();
+        }
+
+        //Constrói a query conectando valores vindo e $_POST
+        $sql = "INSERT INTO produto (nome, preco, quantidade)
+        VALUES ('".$_POST['txtNome']."', '".$_POST['txtPreco']."', '".$_POST['txtQtd']."')";
+        
+        //Try/catch tenta capturar exceções.
+        try{
+        $resultado = $conecta->query($sql);
+        echo '
+        <a href="index.php">
+        <h1 class="w3-button w3-blue">Produto Salvo com sucesso! </h1>
+        </a>
+        ';
+        }catch(PDOException $e){
+        echo '
+        <a href="index.php">
+        <h1 class="w3-button w3-blue">ERRO! </h1>
+        </a>
+        ';
+        }
+        ?>
+
+    </div>
+
+</body>
+</html>
