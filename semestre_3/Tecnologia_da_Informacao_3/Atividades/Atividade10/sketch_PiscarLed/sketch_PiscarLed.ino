ceab76f34a78f23da5d6a0dbe3e0b714c61d2187
Matheus Massuda <mat.massuda@gmail.com>
2026-04-27 02:03:57 -0300

    chore: atualização TCC
    - Agenda05 TCC;

--- semestre_3/Tecnologia_da_Informacao_3/Atividades/Atividade10/sketch_PiscarLed/sketch_PiscarLed.ino
+++ semestre_3/Tecnologia_da_Informacao_3/Atividades/Atividade10/sketch_PiscarLed/sketch_PiscarLed.ino
@@ -6,10 +6,19 @@ void setup() {
 void loop() {
   // Acende LED na porta 13 como ALTO (Corrente)
   digitalWrite (13, HIGH);
-  // Estabelece 1000 milsegundos de "espera" com ele aceso
-  delay (1000);
+  // Estabelece 140 milsegundos de "espera" com ele aceso
+  delay (140);
   // Acende LED na porta 13 como BAIXO == DESLIGADO (Corrente)
   digitalWrite (13, LOW);
-  // Estabelece 1000 milsegundos de "espera" com ele apagado
-  delay (1000);
+  // Estabelece 140 milsegundos de "espera" com ele apagado
+  delay (140);
+
+  // Acende LED na porta 13 como ALTO (Corrente)
+  digitalWrite (13, HIGH);
+  // Estabelece 140 milsegundos de "espera" com ele aceso
+  delay (140);
+  // Acende LED na porta 13 como BAIXO == DESLIGADO (Corrente)
+  digitalWrite (13, LOW);
+  // Estabelece 500 milsegundos de "espera" com ele apagado, para simular "descanço" do coração
+  delay (500);
 }
